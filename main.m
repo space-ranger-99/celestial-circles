2f288ff8cccd115153f1490a63c439e9c01b2973
Alexander Little <corbyn.little@torontomu.ca>
2023-01-28 08:45:38 -0500

    added input validation

--- main.m
+++ main.m
@@ -1,10 +1,41 @@
+clear;
+
 % Keplerian elements
 a = input('Enter the semi-major axis (km): ');
+while ~isnumeric(a) || a < 6371 || a > 200000
+    disp('Error: Input must be a number between 6371 and 200000.')
+    a = input('Enter the semi-major axis (km): ');
+end
+
 e = input('Enter the eccentricity: ');
+while ~isnumeric(e) || e < 0 || e >= 1
+    disp('Error: Input must be a number between 0 and 1')
+    e = input('Enter the eccentricity: ');
+end
+
 i = input('Enter the inclination (degrees): ');
+while ~isnumeric(i) || i < 0 || i > 180
+    disp('Error: Input must be a number between 0 and 180')
+    i = input('Enter the inclination (degrees): ');
+end
+
 RAAN = input('Enter the right ascension of the ascending node (degrees): ');
+while ~isnumeric(RAAN) || RAAN < 0 || RAAN > 360
+    disp('Error: Input must be a number between 0 and 360')
+    RAAN = input('Enter the right ascension of the ascending node (degrees): ');
+end
+
 w = input('Enter the argument of perigee (degrees): ');
+while ~isnumeric(w) || w < 0 || w > 360
+    disp('Error: Input must be a number between 0 and 360')
+    w = input('Enter the argument of perigee (degrees): ');
+end
+
 M0 = input('Enter the mean anomaly at t0 (degrees): ');
+while ~isnumeric(M0) || M0 < 0 || M0 > 360
+    disp('Error: Input must be a number between 0 and 360')
+    M0 = input('Enter the mean anomaly at t0 (degrees): ');
+end
 
 % Time vector for simulation
 t0 = 0; % starting time (s)
